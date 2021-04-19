bf47b0d450817df2c3c2827cfd27f604521877cf
Maykon Lacerda <maykonalvslacerda@gmail.com>
2021-04-19 19:29:14 -0300

    Day 01

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    return res.send("Hello World");
+})
+
+
+app.listen(3333, () => console.log("Server is running on port 3333"));
