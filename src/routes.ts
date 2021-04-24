2a2b3731e049dc8867f04e54f30f0e98766ccc52
Maykon Lacerda <maykonalvslacerda@gmail.com>
2021-04-24 15:16:22 -0300

    create  messages

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { MessagesController } from './controllers/MessagesController'
 import { SettingsController } from './controllers/SettingsContoller'
 import { UsersController } from './controllers/UsersController'
 
@@ -6,8 +7,10 @@ const routes = Router()
 
 const settingsContoller = new SettingsController()
 const usersContoller = new UsersController()
+const messagesController = new MessagesController()
 
 routes.post("/settings", settingsContoller.create)
 routes.post("/user", usersContoller.create)
+routes.post("/messages", messagesController.create)
 
 export { routes }
